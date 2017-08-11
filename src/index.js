627169f4fb0cb84019e3150763f9500412f2a555
Craig Walker <github@softcraft.ca>
2017-08-11 19:27:20 +0000

    Fibbronacci circles for sizes, distance, and hue

--- src/index.js
+++ src/index.js
@@ -2,23 +2,38 @@ require("./index.scss");
 import * as d3 from 'd3';
 
 $(document).ready(() => {
-    const viz = d3.select("#viz");
-    const svg = viz.append("svg");
+    const svg = d3.select("#viz");
 
-    const data = [1, 1];
+    const data = [
+        {value: 1, sum: 1},
+        {value: 1, sum: 2}
+    ];
     for (var index = 2; index <= 5; index++) {
-        data.push(data[index - 1] + data[index - 2]);
+        const p1 = data[index - 1];
+        const p2 = data[index - 2];
+        const value = p1.value + p2.value;
+        data.push({
+            sum: p1.sum + value,
+            value
+        });
     }
+    const total = d3.max(data, (d) => d.sum);
 
+    const width = d3.scaleLinear()
+        .domain([0, total])
+        .range([0, 1]);
+    const hue = d3.scaleLinear()
+        .domain(d3.extent(data, (d) => d.sum))
+        .range([0, 359 - 30]);
+    
     svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cy", 100)
-        .attr("cx", (d) => d * 6 * 20)
+        .attr("cy", 0.5)
+        .attr("cx", (d) => width(d.sum) - (width(d.value) / 2))
+        .style("fill", (d) => d3.hsl(hue(d.sum), 1, 0.5).toString())
         .transition()
-        .attr("r", (d) => d * 20)
+        .attr("r", (d) => width(d.value) / 2)
         ;
-
-    svg.style("width", "100%").style("height", "auto");
 });
