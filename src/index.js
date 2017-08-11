1c56c0a2d5797864cc75a65b282a1579fa5462c8
Craig Walker <github@softcraft.ca>
2017-08-11 21:05:22 +0000

    Assign hues based on area

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 require("./index.scss");
 import * as d3 from 'd3';
 
+function area(diameter) {
+    return Math.pow(Math.PI * (diameter / 2), 2);
+}
+
 $(document).ready(() => {
     const svg = d3.select("#viz");
 
     const data = [
-        {n: 1, value: 1, sum: 1},
-        {n: 2, value: 1, sum: 2}
+        { area: area(1), n: 1, value: 1, sum: 1 },
+        { area: area(1), n: 2, value: 1, sum: 2 }
     ];
-    for (var index = 2; index < 10; index++) {
+    for (var index = 2; index < 12; index++) {
         const p1 = data[index - 1];
         const p2 = data[index - 2];
         const value = p1.value + p2.value;
         data.push({
+            area: area(value),
             n: index + 1,
             sum: p1.sum + value,
             value
         });
     }
     const total = d3.max(data, (d) => d.sum);
+    const totalArea = d3.sum(data, (d) => d.area);
 
     const width = d3.scaleLinear()
         .domain([0, total])
         .range([0, 1]);
     const hue = d3.scaleLinear()
-        .domain(d3.extent(data, (d) => d.n))
+        .domain([0,totalArea])
         .range([0, 359 - 30]);
-    
+
     const minRadius = width(d3.min(data, (d) => d.value)) / 2 / 2;
 
     svg.selectAll("circle")
@@ -36,7 +42,7 @@ $(document).ready(() => {
         .attr("cy", d3.randomNormal())
         .attr("cx", -0.1)
         .attr("r", minRadius)
-        .style("fill", (d) => d3.hsl(hue(d.n), 1, 0.5).toString())
+        .style("fill", (d) => d3.hsl(hue(d.area), 1, 0.5).toString())
         .transition()
         .duration(500)
         .delay((d) => d.n * 250)
