29af0f58838405e5b553c70f7f7adb165e7100da
Craig Walker <github@softcraft.ca>
2017-08-11 04:30:22 +0000

    Set up loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,35 @@
 /* eslint-env node */
+var path = require("path");
 
 module.exports = {
-    context: __dirname,
-    entry: "./src/index.js",
+    context: path.resolve(__dirname, "src"),
+    entry: "./index.js",
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            // loaders: ['babel-loader?presets[]=es2015'],
+            loader: "file-loader",
+            exclude: /(node_modules|bower_components)/
+        }, {
+            test: /\.css$/,
+            loaders: ['style', 'css']
+        }, {
+            test: /\.scss$/,
+            loaders: ['style', 'css', 'postcss', 'sass']
+        }, {
+            test: /\.sass$/,
+            loader: 'style!css!sass?sourceMap'
+        }, {
+            test: /\.woff$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+        }, {
+            test: /\.woff2$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+        }, {
+            test: /\.(eot|ttf|svg|gif|png)$/,
+            loader: "file-loader"
+        }]
+    },
     output: {
         path: __dirname + "/dist",
         filename: "bundle.js"
